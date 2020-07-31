eb159b541731efef087738b6b70a65cc7ed935a5
dingwenbo <dingwenbo@airlook.com>
2020-07-31 22:49:50 +0800

    访问量

--- blog/.vuepress/components/Valine.vue
+++ blog/.vuepress/components/Valine.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="vcomment">
+  <div class="vcomment" v-if="load">
+    <!-- id 将作为查询条件 -->
+    <span :id="url" class="leancloud_visitors" data-flag-title="丁文博的小站">
+      <em class="post-meta-item-text">阅读量</em>
+      <i class="leancloud-visitors-count">loading</i>
+    </span>
     <div id="vcomments"></div>
   </div>
 </template>
@@ -12,13 +17,24 @@ export default {
     //   return this.$page.frontmatter;
     // },
   },
-  mounted: function () {
-    this.createValine();
+  data() {
+    return {
+      load: false,
+      url: "",
+    };
+  },
+  mounted() {
+    this.url = window.location.pathname;
+    this.load = true;
+    this.$nextTick(() => {
+      this.createValine();
+    });
   },
   methods: {
     createValine() {
+      this.url = window.location.pathname;
       const Valine = require("valine");
-    //   window.AV = require("leancloud-storage");
+      //   window.AV = require("leancloud-storage");
       const valine = new Valine({
         el: "#vcomments",
         appId: "ToCSDiTbKzCmgY9F1XU1ct6S-gzGzoHsz",
@@ -26,6 +42,7 @@ export default {
         notify: false,
         verify: false,
         avatar: "monsterid",
+        visitor: true, // 阅读量统计
         path: window.location.pathname,
         placeholder: "欢迎留言与我分享您的想法...",
       });
@@ -44,3 +61,8 @@ export default {
   },
 };
 </script>
+<style  scoped>
+#vcomments {
+  margin-top: 30px;
+}
+</style>
